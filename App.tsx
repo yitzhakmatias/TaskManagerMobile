d30fb396a996f649429435fa7e2ff5444ac9073d
Yitzhak Matias <yitzhak.matias@fleebe.ai>
2025-03-27 23:07:11 -0400

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Button, View, StyleSheet } from "react-native";
 
-export default function App() {
+import { Amplify } from "aws-amplify";
+import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
+
+import outputs from "./amplify_outputs.json";
+
+Amplify.configure(outputs);
+
+const SignOutButton = () => {
+  const { signOut } = useAuthenticator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.signOutButton}>
+        <Button title="Sign Out" onPress={signOut} />
+      </View>
   );
-}
+};
+
+const App = () => {
+  return (
+      <Authenticator.Provider>
+        <Authenticator>
+          <SignOutButton />
+        </Authenticator>
+      </Authenticator.Provider>
+  );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  signOutButton: {
+    alignSelf: "flex-end",
   },
 });
+
+export default App;
